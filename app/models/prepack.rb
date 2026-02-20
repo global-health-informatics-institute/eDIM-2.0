81c58718c2a2a6601ea35fd0d384c61d2f9ffde9
ghii <nyaraihk@gmail.com>
2026-02-20 16:17:21 +0200

    redefine update method to include othe rparams

--- app/models/prepack.rb
+++ app/models/prepack.rb
@@ -3,24 +3,62 @@ class Prepack < ActiveRecord::Base
   belongs_to :drug
   belongs_to :prepacked_by, class_name: 'User'
   belongs_to :location, optional: true
-  validates :drug_id, :location_id, :quantity_per_pack, :num_packs, :total_quantity, presence: true
 
+  has_many :prepack_labels, dependent: :destroy
+
+  # -----------------------
+  # VALIDATIONS
+  # -----------------------
+  validates :drug_id, :location_id, :quantity_per_pack, :num_packs, presence: true
+  validates :pack_identifier, uniqueness: true, allow_nil: true
+
+  validate :cannot_reduce_below_dispensed
+
+  # -----------------------
+  # CALLBACKS
+  # -----------------------
+  before_validation :calculate_total_quantity
+  #before_save :generate_directions
+  after_initialize :set_default_status, if: :new_record?
+
+  # -----------------------
+  # METHODS
+  # -----------------------
   def times
     super || []
   end
-
-  #has_many :prepack_labels, foreign_key: :prepack_id
-  has_many :prepack_labels, dependent: :destroy
-
-  validates :quantity_per_pack, :num_packs, :total_quantity, presence: true
-
-  validates :pack_identifier, uniqueness: true, allow_nil: true
-
-  after_initialize :set_default_status, if: :new_record?
 
   private
 
   def set_default_status
     self.status ||= 'available'
   end
+
+  # Automatically calculate total quantity
+  def calculate_total_quantity
+    self.total_quantity = quantity_per_pack.to_i * num_packs.to_i
+  end
+
+  # Prevent reducing num_packs below already dispensed packs
+  def cannot_reduce_below_dispensed
+    return unless persisted?
+
+    dispensed = prepack_labels.where(dispensed: true, deleted: false, voided: false).count
+    if num_packs.to_i < dispensed
+      errors.add(:num_packs, "cannot be less than already dispensed packs (#{dispensed})")
+    end
+  end
+
+  #def generate_directions
+    #return unless dose.present? && frequency.present? && administration.present?
+
+    #freq_words = { 'OD' => 'Once', 'BD' => 'Two', 'TDS' => 'Three', 'QID' => 'Four' }
+    #route_words = { 'oral' => 'Take', 'topical' => 'Apply', 'injection' => 'Inject', 'respiratory' => 'Inhale' }
+
+    #freq_word  = freq_words[frequency] || 'Two'
+    #route_word = route_words[administration] || 'Take'
+
+    #self.directions = 
+    #"#{route_word} #{dose} #{freq_word} Times A Day"
+  #end
 end
